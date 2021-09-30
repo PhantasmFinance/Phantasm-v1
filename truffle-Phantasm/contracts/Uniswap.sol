00f5a462a3ad53b91360c5d57a512204d855949e
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-09-30 16:54:14 -0700

    It Compiles Now (With Warnings)

--- truffle-Phantasm/contracts/Uniswap.sol
+++ truffle-Phantasm/contracts/Uniswap.sol
@@ -12,8 +12,10 @@ contract Uniswap {
         █──█ █──█ ▀█▀ ▀▀█ █▄█▄█ █▄▄█ █──█ 
         ─▀▀▀ ▀──▀ ▀▀▀ ▀▀▀ ─▀─▀─ ▀──▀ █▀▀▀
     */
-
-    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to) public virtual override {
+    IUniswapV2Router public uniswap = IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    IERC20 public WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    
+    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to) public virtual  {
         IERC20(_tokenIn).approve(address(uniswap), _amountIn);
 
         address[] memory path;
@@ -31,7 +33,7 @@ contract Uniswap {
         uniswap.swapExactTokensForTokens(_amountIn, _amountOutMin, path, _to, block.timestamp);
     }
 
-    function _getAmountOutMin(address _tokenIn, address _tokenOut, uint _amountIn) public virtual override view returns (uint) {
+    function _getAmountOutMin(address _tokenIn, address _tokenOut, uint _amountIn) public virtual  view returns (uint) {
         address[] memory path;
         if (_tokenIn == address(WETH) || _tokenOut == address(WETH)) {
             path = new address[](2);
