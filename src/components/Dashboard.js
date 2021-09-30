0923383b2adb200bda6dcba5b47f1e2a403e8fcd
chinmaygopal931 <chinmayg015@Gmail.com>
2021-09-30 09:36:02 +0530

    ui stuff

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 import { Form,  Button, List } from "antd";
 import { Card} from 'antd';
-import { Slider, Switch } from 'antd';
+import { Slider, Avatar, Input, Space } from 'antd';
 import "./Dashboard.scss"
 import { Footer } from 'antd/lib/layout/layout';
 import { Select } from 'antd';
@@ -19,6 +19,8 @@ export default function Dashboard(){
   const [isLong, setIsLong] = useState(true);
   const [leverage, setLeverage] = useState(5);
   const [symbol, setSymbol] = useState("ETHDAI");
+    const [currentValue, setCurrentValue] = useState(0)
+
  
   return (
     
@@ -29,98 +31,100 @@ export default function Dashboard(){
 
             <div class="sidebar">
                 <div>
-                  <Select className="select" style={{ backgroundColor: "#a17fe0" }} placeholder="Asset 1" onChange={handleChange}>
+                  <Select className="select" style={{ backgroundColor: "#a17fe0", marginBottom: "20px"}} defaultValue="Asset 1" onChange={handleChange}>
                   </Select>
 
-                  <Select className="select" style={{ backgroundColor: "#a17fe0" }} placeholder="Asset 2" onChange={handleChange}>
+                  <Select className="select" style={{ backgroundColor: "#a17fe0" ,marginBottom: "50px" }} defaultValue="Asset 2" onChange={handleChange}>
                   </Select>                  
                 </div>
-
-                <List
-                  bordered
-                  size="small"
-                  style={{ borderRadius: "10px", marginBottom: "30px" , marginTop: "20px", width: "500px"}}
+                <Form
+                    layout="vertical"
+                    autoComplete="off"
+                    class="form"
                 >
-                  <List.Item>
-                    <List.Item.Meta title={"Amount"}></List.Item.Meta>
-                    <div>{ "-"}</div>    
-                  </List.Item>
-                  <List.Item>
-                    <List.Item.Meta title={"Collateral"}></List.Item.Meta>
-                    <div>{ "-"}</div>
-                  </List.Item>
-                </List>
-                <div className="long-short-box">
-                  <Button
-                    type="text"
-                    shape="round"
-                    style={{
-                      background: isLong ? "#28A644" : "#FFFFFF",
-                      color: isLong ? "#FFFFFF" : "#000000",
-                      borderRadius: "25px 0px 0px 25px",
-                      border: "1px solid #E5E5E5",
-                      borderRight: "none",
-                      boxShadow: "none",
-                      flexGrow: 1
-                    }}
-                    onClick={() => setIsLong(true)}
-                  >
-                    Long
-                  </Button>
-                  <Button
-                    type="text"
-                    shape="round"
-                    style={{
-                      background: isLong ? "#FFFFFF" : "#E54848",
-                      color: isLong ? "#000000" : "#FFFFFF",
-                      borderRadius: "0px 25px 25px 0px",
-                      border: "1px solid #E5E5E5",
-                      borderLeft: "none",
-                      boxShadow: "none",
-                      flexGrow: 1
-                    }}
-                    onClick={() => setIsLong(false)}
-                  >
-                    Short
-                  </Button>
-                </div>
+                    <div style={{ overflow: 'hidden', width: "500px" }}>
+                      <Form.Item
+                        label="Collateral"
+                      >
+                        <Input placeholder="Collateral" />
+                      </Form.Item>
+                    </div>
+                </Form>
 
-                <Form>
-                  <div>
-                    <p>
-                      Leverage{" "}
-                    </p>
-                    <Slider trackStyle={{ backgroundColor: "green" }}
-                            handleStyle={{ borderColor:"green" }} 
-                            defaultValue={0}
-                            min={0}
-                            max={10}
-                    />
-
-                  </div>
-                  <Form.Item>
-      
-                  </Form.Item>
-                  <Form.Item>
-                    <div className="submit-box">
+                  <div className="long-short-box">
                     <Button
-                      type="primary"
-                      htmlType="submit"
-                      size="large"
+                      type="text"
+                      shape="round"
                       style={{
-                        background:"#28A644",
+                        background: isLong ? "#28A644" : "#FFFFFF",
+                        color: isLong ? "#FFFFFF" : "#000000",
+                        borderRadius: "25px 0px 0px 25px",
                         border: "1px solid #E5E5E5",
+                        borderRight: "none",
                         boxShadow: "none",
                         flexGrow: 1,
-                        borderRadius: "10px"
                       }}
+                      onClick={() => setIsLong(true)}
                     >
-                      Submit
+                      Long
                     </Button>
+                    <Button
+                      type="text"
+                      shape="round"
+                      style={{
+                        background: isLong ? "#FFFFFF" : "#E54848",
+                        color: isLong ? "#000000" : "#FFFFFF",
+                        borderRadius: "0px 25px 25px 0px",
+                        border: "1px solid #E5E5E5",
+                        borderLeft: "none",
+                        boxShadow: "none",
+                        flexGrow: 1
+                      }}
+                      onClick={() => setIsLong(false)}
+                    >
+                      Short
+                    </Button>
+                  </div>
+
+                  <Form>
+                    <div class="slider-box">
+                      <p>
+                        Leverage:   { currentValue}x
+                      </p>  
+                                          
+                      <Slider 
+                          defaultValue={0} 
+                          disabled={false} 
+                          trackStyle={{ backgroundColor: "green" }}
+                          handleStyle={{ borderColor:"green" }}   
+                          max={10} 
+                          onChange={(value)=> { setCurrentValue(value)
+                                    }}/>
+
                     </div>
-                  </Form.Item>
+                    <Form.Item>
+        
+                    </Form.Item>
+                    <Form.Item>
+                      <div className="submit-box">
+                      <Button
+                        type="primary"
+                        htmlType="submit"
+                        size="large"
+                        style={{
+                          background:"#28A644",
+                          boxShadow: "none",
+                          width: 500,
+                          borderRadius: "10px"                
+                        }}
+                      >
+                        Submit
+                      </Button>
+                      </div>
+                    </Form.Item>
                 </Form>
-          </div>      
+            </div>     
+             
             <TradingViewWidget
                   symbol="ETHUSD"
                   theme={Themes.DARK}
@@ -134,22 +138,21 @@ export default function Dashboard(){
 
           <Card
               className="card"
-              title="88Mph & USDC"
+              title="Insulated Bond"
               style={{
                 borderRadius: '25px',
                 padding: '15px',
                 backgroundColor:"#a17fe0",
                 borderColor:"#a17fe0"
-            }}             
-
-              cover={<img alt="example" class="imginside" src="https://c.gitcoin.co/grants/b497f01f46c8a5ea4d3d1cdbff76348e/Frame_102_jpg.jpg" height="250px" />}
-              
+            }}                           
           >
-            <Meta
-                className="Meta"
-                title="Card title"
-                description="This is the description"
-            />
+              <div class="row-card">
+                <p>Asset 1 : {} </p>
+              </div>
+
+              <div class="row-card">
+                <p>Asset 2 : {} </p>
+              </div>
               <div class="card-leverage-slider">
                     <p>
                       Leverage{" "}
@@ -167,9 +170,7 @@ export default function Dashboard(){
         </div>
       </main>
 
-      <Footer class="site-footer">
-           <div className="footer-copyright">&copy; 2021 Phantasm Finance</div>
-      </Footer>
+
 
     </div>
   );
