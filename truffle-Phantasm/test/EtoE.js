ea6235b4abc920e22062b25f59d05d5f4fac452c
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-02 16:04:45 -0600

    Update EtoE.js

--- truffle-Phantasm/test/EtoE.js
+++ truffle-Phantasm/test/EtoE.js
@@ -5,21 +5,29 @@ const BigNumber = require('bignumber.js');
 const IERC20 = artifacts.require("IERC20");
 const Phantasm = artifacts.require("PhantasmManager");
 const Uniswapper = artifacts.require("Uniswap"); 
+const CErc20 = artifacts.require("CErc20"); 
+
 const CompoundLender = artifacts.require("CompoundImplementation");
 
 
 contract("Phantasm", (accounts) => {
     beforeEach(async () => {
       // Setup Chain env
-      PhantasmManager = await Phantasm.new({"from" : "0xF977814e90dA44bFA03b6295A0616a897441aceC"});
+      PhantasmManager = await Phantasm.new({"from" : "0x66c57bF505A85A74609D2C83E94Aabb26d691E1F"});
+
+      erc20WBTC = await IERC20.at("0x2260fac5e5542a773aa44fbcfedf7c193bc2c599");
+
+      erc20DAI = await IERC20.at("0x6B175474E89094C44Da98b954EedeAC495271d0F") 
+      
+      cerc20DAI = await CErc20.at("0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643")
 
       UniswapImplementation = await Uniswapper.new();
 
       CompoundImplementation = await CompoundLender.new();
 
-      PhantasmManager.addSwapImplementation(UniswapImplementation.address, {"from" : "0xF977814e90dA44bFA03b6295A0616a897441aceC"});
+      await PhantasmManager.addSwapImplementation(UniswapImplementation.address, {"from" : "0x66c57bF505A85A74609D2C83E94Aabb26d691E1F"});
 
-      PhantasmManager.addLenderImplementation(CompoundImplementation.address, {"from" : "0xF977814e90dA44bFA03b6295A0616a897441aceC"});
+      await PhantasmManager.addLenderImplementation(CompoundImplementation.address, {"from" : "0x66c57bF505A85A74609D2C83E94Aabb26d691E1F"});
 
     });
   
@@ -33,9 +41,14 @@ contract("Phantasm", (accounts) => {
         uint256 _borrowAmount,
         uint256 _borrowFactor
       */
-      WBTC = IERC20.at("0x2260fac5e5542a773aa44fbcfedf7c193bc2c599");
-      Value = new BigNumber("")
-      WBTC.approve(PhantasmManager.address, 0xfffffff);
-      PhantasmManager.openLongPositionNFT(0,0, "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", 1000, 150, {"from" : "0xF977814e90dA44bFA03b6295A0616a897441aceC"});
+      let borrowMe = new BigNumber("100000000000000000000")
+
+
+      let AssetAmount = new BigNumber("11000000000000000000000")
+
+      erc20DAI.approve(PhantasmManager.address, AssetAmount, {"from" : "0x66c57bF505A85A74609D2C83E94Aabb26d691E1F"});
+
+
+      await PhantasmManager.openLongPositionNFT(0,0, "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", borrowMe, 50, AssetAmount ,{"from" : "0x66c57bF505A85A74609D2C83E94Aabb26d691E1F"});
     });
   });
