6e0030ee9a583b925d20c3c65fcbbf8149cb0119
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-04 09:23:07 -0600

    Update swapImplementation.sol

--- truffle-Phantasm/contracts/interfaces/swapImplementation.sol
+++ truffle-Phantasm/contracts/interfaces/swapImplementation.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 interface swapImplementation {
-    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to, address[] memory _swapRoute) external returns (uint);
+    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to) external returns (uint);
 
     function _getAmountOutMin(address _tokenIn, address _tokenOut, uint _amountIn) external view returns (uint);
 }
