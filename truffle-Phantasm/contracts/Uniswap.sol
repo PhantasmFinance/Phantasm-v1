7c525e5b9e82a2ab18e42ec31992bfa297b30e72
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-04 20:31:12 -0600

    V3 now

--- truffle-Phantasm/contracts/Uniswap.sol
+++ truffle-Phantasm/contracts/Uniswap.sol
@@ -1,56 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
+pragma abicoder v2;
 
-import './interfaces/IPhantasm.sol';
-import './interfaces/ICompound.sol';
 import './interfaces/IUniswap.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
+// Thank the Uniswap docs for this last minute migration
 contract Uniswap {
     /*
         █──█ █▀▀▄ ─▀─ █▀▀ █───█ █▀▀█ █▀▀█ 
         █──█ █──█ ▀█▀ ▀▀█ █▄█▄█ █▄▄█ █──█ 
         ─▀▀▀ ▀──▀ ▀▀▀ ▀▀▀ ─▀─▀─ ▀──▀ █▀▀▀
-    */
-    IUniswapV2Router public uniswap = IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-    IERC20 public USDC = IERC20(0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48);
+    */    
+
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;    
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    // For this we will set the pool fee to 0.3%.    
+    int24 public constant poolFee = 3000;
     
-    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to) public {
-        IERC20(_tokenIn).approve(address(uniswap), _amountIn);
+    int24 public constant stablefee = 3000;
+    ISwapRouter public constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
-        address[] memory path;
-        if (_tokenIn == address(USDC) || _tokenOut == address(USDC)) {
-            path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;
-        } else {
-            path = new address[](3);
-            path[0] = _tokenIn;
-            path[1] = address(USDC);
-            path[2] = _tokenOut;
+    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to) public returns (uint256 amountOut) {
+
+        TransferHelper.safeApprove(_tokenIn, address(swapRouter), _amountIn);
+
+        // Multiple pool swaps are encoded through bytes called a `path`. A path is a sequence of token addresses and poolFees that define the pools used in the swaps.
+        // The format for pool encoding is (tokenIn, fee, tokenOut/tokenIn, fee, tokenOut) where tokenIn/tokenOut parameter is the shared token across the pools.
+        // Since we are swapping DAI to USDC and then USDC to WETH9 the path encoding is (DAI, 0.3%, USDC, 0.3%, WETH9).
+        uint amountOutMin;
+
+        if(_amountOutMin == 0){
+            uint amountOutMin = 0;
+        } else{
+            uint amountOutMin = _amountOutMin;
         }
+        ISwapRouter.ExactInputParams memory params =
+            ISwapRouter.ExactInputParams({
+                path: abi.encodePacked(_tokenIn, stablefee, USDC, poolFee, _tokenOut),
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: _amountIn,
+                amountOutMinimum: amountOutMin
+            });
 
-        uniswap.swapExactTokensForTokens(_amountIn, _amountOutMin, path, _to, block.timestamp);
-
-        IERC20(_tokenOut).transfer(msg.sender, IERC20(_tokenOut).balanceOf(address(this)));
+        // Executes the swap.
+        uint tokensOut = swapRouter.exactInput(params);
+        IERC20(_tokenOut).transfer(_to,tokensOut);
+        return tokensOut;
     }
-
-    function _getAmountOutMin(address _tokenIn, address _tokenOut, uint _amountIn) public view returns (uint) {
-        address[] memory path;
-        if (_tokenIn == address(USDC) || _tokenOut == address(USDC)) {
-            path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;
-        } else {
-            path = new address[](3);
-            path[0] = _tokenIn;
-            path[1] = address(USDC);
-            path[2] = _tokenOut;
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
         }
-
-        // same length as path
-        uint[] memory amountOutMins = uniswap.getAmountsOut(_amountIn, path);
-
-        return amountOutMins[path.length - 1];
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
