5362d0a212214e572013bbb046fe6528de613957
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-07 20:30:38 -0600

    Update EEIntegration.sol

--- truffle-Phantasm/contracts/EEIntegration.sol
+++ truffle-Phantasm/contracts/EEIntegration.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import './interfaces/IPhantasm.sol';
-import './interfaces/ICompound.sol';
-import './interfaces/IUniswap.sol';
+
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './interfaces/IDInterest.sol';
-
-contract EEIntegration {
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+contract EEIntegration is ERC1155Holder {
 
     IERC20 public WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IERC20 public dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     CErc20 public cDai = CErc20(0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643);
     DInterest public daiViaAavePool = DInterest(0x6D97eA6e14D35e10b50df9475e9EFaAd1982065E);
     DInterest public daiViaCompoundPool = DInterest(0x11B1c87983F881B3686F8b1171628357FAA30038);
+    address PhantasmManager;
 
     mapping (address => mapping (address => uint64[])) public myFundingIDs; // Pool Address => User Address => Funding Id
 
+    constructor(address _phantasmManager) {
+        PhantasmManager = _phantasmManager; 
+    }
     function findAndBuyYieldTokens(address _token, uint256 _amount) public returns (uint256 _amountSpent) {
         uint256 amountLeft = _amount;
         if (_token == address(dai)) {
@@ -57,6 +59,8 @@ contract EEIntegration {
                 }
             }
         }
+        // Works because _token is DAI, but IIRC interest is accured in the actual asset, not DAI
+        IERC20(_token).transfer(PhantasmManager, amountCollected);
         return amountCollected;
     }
 
@@ -80,7 +84,7 @@ contract EEIntegration {
         return insuredAmount;
     }
 
-    // NEEDS TO BE REVISED => thisYTAmount should be the *Price* to buy the yield tokens => not the total amount of Yield Tokens
+    // NEEDS TO BE REVISED => thisYTPrice should be the *Price* to buy the yield tokens => not the total amount of Yield Tokens
     function getTotalYTInBudget (address _token, uint256 _amountIn) public view returns (uint256 _insuranceAvailable) {
         uint256 insuranceAvailable = 0;
         uint256 amountLeft = _amountIn;
@@ -93,10 +97,10 @@ contract EEIntegration {
             for (uint256 i = 0; i < daiPools.length; i++) {
                 if (daiPools[i].depositsLength() != 0) {
                     for (uint64 j = 1; j <= daiPools[i].depositsLength(); j++) {
-                        uint256 thisYTAmount = getYTAvailableFromDeposit(address(daiPools[i]), j);
-                        if (amountLeft > thisYTAmount && daiPools[i].getDeposit(j).fundingID == 0) {
-                            amountLeft = amountLeft - thisYTAmount; // THIS IS WRONG -> thisYTAmount needs to be replaced with the PRICE of thisYTAmount
-                            insuranceAvailable = insuranceAvailable + thisYTAmount; // SAME GOES FOR THIS
+                        uint256 thisYTPrice = getYTPriceFromDeposit(address(daiPools[i]), j);
+                        if (amountLeft > thisYTPrice && daiPools[i].getDeposit(j).fundingID == 0) {
+                            amountLeft = amountLeft - thisYTPrice;
+                            insuranceAvailable = insuranceAvailable + thisYTPrice;
                         }
                     }
                 }
@@ -106,12 +110,9 @@ contract EEIntegration {
     }
 
     /*
-
-
 ░█─░█ █▀▀ █── █▀▀█ █▀▀ █▀▀█ █▀▀ 
 ░█▀▀█ █▀▀ █── █──█ █▀▀ █▄▄▀ ▀▀█ 
 ░█─░█ ▀▀▀ ▀▀▀ █▀▀▀ ▀▀▀ ▀─▀▀ ▀▀▀
-
     */
 
     function buyYieldTokens (address _assetPool, uint64 _depositId, uint256 _stableInAmount) public returns (uint64 _fundingID, uint256 fundingMultitokensMinted, uint256 actualFundAmount, uint256 principalFunded) {
@@ -130,7 +131,7 @@ contract EEIntegration {
         return DInterest(_assetPool).getFunding(_fundingID).principalPerToken;
     }
 
-    function getYTAvailableFromDeposit(address _assetPool, uint64 _depositID) public view returns (uint256) {
-        return DInterest(_assetPool).getDeposit(_depositID).virtualTokenTotalSupply;
-    }   
+    function getYTPriceFromDeposit(address _assetPool, uint64 _depositID) public view returns (uint256) {
+        return (DInterest(_assetPool).getDeposit(_depositID).interestRate / DInterest(_assetPool).getDeposit(_depositID).maturationTimestamp - block.timestamp) * DInterest(_assetPool).getDeposit(_depositID).virtualTokenTotalSupply;
+    }
 }
