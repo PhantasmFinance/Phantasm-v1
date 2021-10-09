bb487d39014355562dc1fb2dbd180c72e3584e46
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-08 18:44:39 -0600

    Update Aave.js

--- truffle-Phantasm/test/Aave.js
+++ truffle-Phantasm/test/Aave.js
@@ -71,21 +71,39 @@ contract("AaveLender", (accounts) => {
 
 
     it("Create a Short Position", async () => {
-      // Iterative Tests here
-
+      /*
+        Create and close a Short Position through Phantasm Manager
+      */
 
       let AssetAmount = new BigNumber("11000000000000000000000")
 
-      LINK.approve(aaveLendingPool.address, AssetAmount, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"})
+      LINK.approve(PhantasmManager.address, AssetAmount, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"})
 
       let borrowMe = new BigNumber("500000000000000000000")
 
+      let accuredInterest = new BigNumber("100000000000000")
+
       let maxApproval = new BigNumber("99999999999999999999999999999999999999999")
 
-      erc20DAI.approve(aaveLendingPool.address, AssetAmount, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"});
+      erc20DAI.approve(PhantasmManager.address, AssetAmount, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"});
+      /*
+        uint64 _lenderImplementation, 
+        uint64 _swapImplementation,
+        address _shortToken,
+        uint256 _borrowFactor,
+        uint256 _assetAmount,
+        uint256 _initialBorrow
+      */
+      await PhantasmManager.openShortPositionNFT(0, 0, "0x514910771af9ca656af840dff83e8264ecf986ca", 50 ,AssetAmount, borrowMe, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"});
+      
+      console.log("Short Position created")
 
-      await aaveLendingPool.leverageShort("0x514910771af9ca656af840dff83e8264ecf986ca", UniswapImplementation.address,AssetAmount, borrowMe, 50, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"});
+      LINK.approve(PhantasmManager.address ,maxApproval, {"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"})
+
+      await PhantasmManager.closeShortPositionNFT(1, 0,accuredInterest,{"from" : "0x28C6c06298d514Db089934071355E5743bf21d60"})
+
  
+    });
     });
     it("Should Created and Close and Insulated Position", async () => {
 
