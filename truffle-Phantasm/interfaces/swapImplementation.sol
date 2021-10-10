a668a7df11ebb18c1b1a11f97556c4670cd5136a
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-10 17:44:31 -0600

    Delete truffle-Phantasm/interfaces directory

--- truffle-Phantasm/interfaces/swapImplementation.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
-interface swapImplementation {
-    function swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to) external;
-
-    function _getAmountOutMin(address _tokenIn, address _tokenOut, uint _amountIn) external view returns (uint);
-}
